7f3609dbbdaa21737b600551d758ed517058e429
Koray ÇELİK <97339180+kryC1@users.noreply.github.com>
2022-09-26 20:45:00 +0300

    Delete apps directory

--- apps/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_view
-from django.contrib.auth.views import LogoutView
-
-urlpatterns = [
-    path('login/', login_view, name="login"),
-    path("logout/", LogoutView.as_view(), name="logout")
-]
